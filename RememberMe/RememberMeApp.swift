737daed9094f7a7d5ead7a37fc953ce8544a76fa
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-03-15 19:03:54 +0100

    Initial Commit

---
+++ RememberMe/RememberMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberMeApp.swift
+//  RememberMe
+//
+//  Created by Oliwier Kasprzak on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RememberMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
